2e5cea7c73f534b2db8599dcceba88813cbdd6d5
Omar Anshasi <OMAR.ANSHASI.2014@mumail.ie>
2016-10-18 15:01:22 +0200

    Added swipe start application
    Change-Id: I96d3b35e57a16793bac16808f11e5c74b023a91d

--- LeapMotion/src/processing/Page.java
+++ LeapMotion/src/processing/Page.java
@@ -13,9 +13,9 @@ import g4p_controls.GEditableTextControl;
 import g4p_controls.GEvent;
 import g4p_controls.GImageButton;
 import g4p_controls.GLabel;
+import g4p_controls.GTextArea;
 import g4p_controls.GTextField;
 import processing.core.PApplet;
-import processing.core.PFont;
 import recording.SignClassifier;
 
 public class Page extends PApplet{
@@ -23,8 +23,6 @@ public class Page extends PApplet{
 	private final int stateWelcomeScreenDisplay=0;
 	final private int stateShowInstructions= 1;
 	private int stateOfProgram = stateWelcomeScreenDisplay;
-	
-	PFont f;                           // STEP 1 Declare PFont variable
 	
 	private final Controller controller = new Controller();
 	private final SignClassifier signClass= new SignClassifier();
@@ -35,8 +33,8 @@ public class Page extends PApplet{
 	@SuppressWarnings("unused")
 	private GImageButton imgButton; 
 	private GTextField textfield; 
-	private GButton startButton;
 	private GLabel welcomeLabel;
+	private GTextArea textarea1; 
 	
 	 public static void main(String[] args) {
 	        PApplet.main("processing.Page");
@@ -48,12 +46,12 @@ public class Page extends PApplet{
 	    
 	    public void setup(){    	
 	    	  createWelcomeGUI();
-	    	f = createFont("Arial",16,true); // STEP 2 Create Font
 	    }
 
 	public void draw(){
 	  switch(stateOfProgram) {
 	case stateWelcomeScreenDisplay:
+		checkIfHandPlacedOverLeap();
 	   break;
 	case stateShowInstructions:
 	  // displayLeapInfo();
@@ -91,10 +89,15 @@ public class Page extends PApplet{
 	  }
 	}
 	
+	private void checkIfHandPlacedOverLeap(){
+	  Frame frame = controller.frame();
+	  if(frame.hands().count()>0){
+		  stateOfProgram=stateShowInstructions;
+	  }
+	}
+	
 	private void createWelcomeGUI(){
-		  G4P.messagesEnabled(true);
 		  G4P.setGlobalColorScheme(GCScheme.BLUE_SCHEME);
-		  G4P.setCursor(ARROW);
 		  surface.setTitle("Sketch Window");
 		  welcomeLabel = new GLabel(this, 161, 67, 80, 100);
 		  welcomeLabel.setFont(new Font("Dialog", Font.PLAIN, 16));
@@ -102,11 +105,10 @@ public class Page extends PApplet{
 		  welcomeLabel.setText("Welcome");
 		  welcomeLabel.setTextBold();
 		  welcomeLabel.setOpaque(false);
-		  startButton = new GButton(this, 129, 136, 150, 30);
-		  startButton.setText("Click to Start");
-		  startButton.setTextBold();
-		  startButton.setLocalColorScheme(GCScheme.RED_SCHEME);
-		  startButton.addEventHandler(this, "handleButtonEvents");
+		  textarea1 = new GTextArea(this, 162, 135, 160, 80, G4P.SCROLLBARS_NONE);
+		  textarea1.setText("Place your hand over the Leap Motion to begin!");
+		  textarea1.setOpaque(true);
+		  textarea1.addEventHandler(this, "textarea1_change1");
 		}
 	
 	private void createSignAlphabetGUI(){
@@ -121,11 +123,17 @@ public class Page extends PApplet{
 	public void handleButtonEvents(GButton button, GEvent event) { 
 		 println("button1 - GButton >> GEvent." + event + " @ " + millis());
 		 welcomeLabel=null;
-		 startButton=null;
+		 textarea1=null;
 			stateOfProgram=stateShowInstructions;
 	}
 	
-	public void handleButtonEvents(GImageButton button, GEvent event) { /* Not called*/ }
+	public void handleButtonEvents(GImageButton button, GEvent event) { /* Not called*/ 
+		if(this.currentLetterPosition==26)
+			this.currentLetterPosition=0;
+		this.currentLetterPosition++;
+	}
 
 	public void handleTextEvents(GEditableTextControl textcontrol, GEvent event) { /* Not called */ }
+	
+	public void textarea1_change1(GTextArea textarea, GEvent event) { /* Not called */ }
 }
